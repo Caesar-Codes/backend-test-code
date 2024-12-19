319e626feb30d74299fddd6d6986bacffc027fc9
Shaurya Dwivedi <shauryadwivedi0108@gmail.com>
2024-12-18 21:49:32 -0600

    Update main.py

--- server/main.py
+++ server/main.py
@@ -33,7 +33,7 @@ async def read_root():
 
 @app.get("/status")
 async def read_root():
-    return {"message": "Mock test"}
+    return {"message": "Mock test 2"}
 
 @app.get("/api/data", response_model=List[Item])
 async def get_data():
